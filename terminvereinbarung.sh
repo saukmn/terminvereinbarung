44dccb831061f6ac02cf3161b8c2b9a2fc31ad00
Saulius Kaminskas <saulius.kaminskas@gmail.com>
2015-07-13 06:56:52 +0200

    Initial commit

---
+++ terminvereinbarung.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+. terminvereinbarung.cfg 
+
+function usage {
+  cat << MSG
+$(basename "$0") [OPTIONS]
+  --help
+    Shows this help message.
+  -d, --datum
+    Just open browser with appointment calendar if available time apears. This is default.
+  -u, --uhrzeiht
+    Navigate directly to available appointment times for the earliest date. NOT IMPLEMENTED YET.
+  -b, --buchung
+    Take first available time to register for appointment. NOT IMPLEMENTED YET.
+MSG
+}
+
+function main {
+
+  [[ $# -gt 0 ]] && [[ "$1" == "--help" ]] && usage && exit
+
+  while true
+  do
+    echo "#$((++num_of_tries)) try at `date`"
+
+    [[ "`curl ${DATUM_URL}`" =~ ${PATTERN} ]] && act $@
+
+    sleep ${DELAY}
+  done
+}
+
+function act {
+  [[ $# -gt 0 ]] && {
+    case "$1" in
+      -u | --uhrzeit) 					
+        # navigate to
+        echo Navigaiting to earliest available appointment times
+        open_urhzeit_url
+        ;;
+      -b | --buchung)
+        # I'm too busy now, do it for me
+        echo Trying to book appointment automatically
+        book
+        ;; 
+      *)
+        # just open the browser
+        echo Opening appointment calendar
+        open_datum_url
+    esac
+  } || open_datum_url
+}
+
+function open_datum_url {
+  ${BROWSER} ${DATUM_URL}
+}
+
+function open_urhzeit_url {
+  false || open_datum_url
+}
+
+function book {
+  false || open_urhzeit_url
+}
+
+main $@
